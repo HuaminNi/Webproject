6db6a28a8a74a395434cc52cc5f6efce6c9f6c9b
Huamin Ni <imsophieni@gmail.com>
2020-03-10 15:38:56 +0800

    bootstraped the comment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser')
+var path = require('path')
 
 const mongoose = require('mongoose')
 mongoose.connect("mongodb://myUserAdmin:admin123@localhost/yelp_camp?authSource=admin", { useNewUrlParser: true })
@@ -9,6 +10,7 @@ var Restaurant = require('./models/restaurants')
 var Comment = require('./models/comment')
 
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(__dirname+'/public'))
 
 
 
